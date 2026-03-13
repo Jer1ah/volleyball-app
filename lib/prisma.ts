621ab5788bb296783aff7d14ddc5ed4733dc415d
Jeriah Bowers <jbowers@webstaurantstore.com>
2026-03-13 14:03:25 -0400

    Fixing Errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
 
-const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL;
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// On Vercel, these variables are injected automatically
+const connectionString = process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL
+
 const pool: any = new pg.Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({ adapter }) // No URL here, the adapter handles it!
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
